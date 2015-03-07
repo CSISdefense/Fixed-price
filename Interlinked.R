481351b949bb3aaac63c3cce160a51adb41cb00a
Greg Sanders <gregory.sanders@gmail.com>
2015-03-07 11:43:43 -0500

    Updates to decrease number of rows.

--- Interlinked.R
+++ Interlinked.R
@@ -3,7 +3,7 @@
 #date: "Tuesday, March 03, 2015"
 
 Interlinked  <- read.csv(
-    "Office_cachedCSIScontractIDtoContractingOfficeIDlikelyInterlinkedPlatform.csv",
+    "C:\\Users\\Greg Sanders\\Documents\\Development\\Fixed-price\\Data\\Office_cachedCSIScontractIDtoContractingOfficeIDlikelyInterlinkedPlatform.csv",
     header = TRUE, sep = ",", dec = ".", strip.white = TRUE, 
     na.strings = c("NULL","NA",""),
     stringsAsFactors = TRUE
@@ -14,23 +14,50 @@ colnames(Interlinked)[colnames(Interlinked)=="ï..CSIScontractID"]<-"CSIScontract
 #Eliminate missing values. If this eliminates contracts entirely, that's okay, that means they've got no interlinked
 Interlinked<-subset(Interlinked,!is.na(PlatformPortfolio)&
                         !is.na(ContractingOfficeID)&
-                        !is.na(MinofSignedDate)&
-                        !is.na(LastUltimateCompletionDate))
+                        !is.na(MinofSignedDate))
 
 #Rather than deal with duplicate counting, I'm just treating contracts with 
 #multiple contracting office and platform portfolio dyads as fractional. The more
 #you link with them, the more likely it is you're interrelated
-Interlinked$InverseVariants<-i/Interlinked$Variants
+Interlinked$InverseVariants<-1/Interlinked$Variants
 
 #Convert the date columns from text to date
 Interlinked$MinofSignedDate<-strptime(Interlinked$MinofSignedDate,"%Y-%m-%d")
 Interlinked$LastUltimateCompletionDate<-strptime(Interlinked$LastUltimateCompletionDate,"%Y-%m-%d")
+
+DateCutoff<-strptime("2006-10-01","%Y-%m-%d")
+
+#Drop all contracts that concluded before 
+Interlinked<-subset(Interlinked,is.na(Interlinked$LastUltimateCompletionDate)|
+                        Interlinked$LastUltimateCompletionDate>=DateCutoff)
+write.table(Interlinked
+            ,file=   "C:\\Users\\Greg Sanders\\Documents\\Development\\Fixed-price\\Data\\Office_cleanedCSIScontractIDtoContractingOfficeIDlikelyInterlinkedPlatform.csv"
+            , sep=","
+            , row.names=FALSE
+            , append=FALSE
+)
+
+#END preprocessing I've alredy done
+
+
+#Start the bit I can't run in R absent serious optimization
+Interlinked  <- read.csv(
+    "C:\\Users\\Greg Sanders\\Documents\\Development\\Fixed-price\\Data\\Office_cleanedCSIScontractIDtoContractingOfficeIDlikelyInterlinkedPlatform.csv",
+    header = TRUE, sep = ",", dec = ".", strip.white = TRUE, 
+    na.strings = c("NULL","NA",""),
+    stringsAsFactors = TRUE
+)
+
+
+
 
 #Create a subset of only contracts that don't have any system equipment codes
 InterlinkedNoSEC<-subset(interlinked,is.na(AnyIdentifiedSystemEquipment)|AnyIdentifiedSystemEquipment==0)
 
 #The big loop 
 for(i in i..nrow(Interlinked)){
+    #Skip rows that start prior to 10/1/2006
+    if(Interlinked[i,"MinofSignedDate"]>=DateCutoff)
     #If you've got any systemequipment codes, only compare with those contracts without.
     #No self matches for this set.
     if(Interlinked[i,"AnyIdentifiedSystemEquipment"]==1){
@@ -38,7 +65,9 @@ for(i in i..nrow(Interlinked)){
             InterlinkedNoSEC$ContractingOfficeID==Interlinked[i,"ContractingOfficeID"]&
                 InterlinkedNoSEC$PlatformPortfolio==Interlinked[i,"PlatformPortfolio"]&
                 InterlinkedNoSEC$MinofSignedDate<=Interlinked[i,"MinofSignedDate"]&
-                InterlinkedNoSEC$LastUltimateCompletionDate>=Interlinked[i,"MinofSignedDate"]
+                (is.na(InterlinkedNoSEC$LastUltimateCompletionDate)|
+                     InterlinkedNoSEC$LastUltimateCompletionDate>=Interlinked[i,"MinofSignedDate"]
+                )
             ])
         
     }#Otherwise compare with everything, subtract one to cancel out self matches.
@@ -47,7 +76,9 @@ for(i in i..nrow(Interlinked)){
             Interlinked$ContractingOfficeID==Interlinked[i,"ContractingOfficeID"]&
                 Interlinked$PlatformPortfolio==Interlinked[i,"PlatformPortfolio"]&
                 Interlinked$MinofSignedDate<=Interlinked[i,"MinofSignedDate"]&
-                Interlinked$LastUltimateCompletionDate>=Interlinked[i,"MinofSignedDate"]
+                (is.na(Interlinked$LastUltimateCompletionDate)|
+                     Interlinked$LastUltimateCompletionDate>=Interlinked[i,"MinofSignedDate"]
+                )
             ])-1
     }
 }
@@ -58,7 +89,6 @@ aggregate(Interlinked$InverselinkedCount,by=list(Interlinked$CSIScontractID))
 #Create a new CSV
 write.table(InterlinkedOutput
             ,file="Office_platform_interlinked_processed.csv"                  
-            ,header=TRUE
             , sep=","
             , row.names=FALSE
             , append=FALSE
